68746c142f1983bdd3c89693370678ef96d8a011
Eli-Ledford <fazeford22@gmail.com>
2025-02-06 11:07:11 -0500

    new upe background image

--- main.py
+++ main.py
@@ -38,10 +38,10 @@ def open_homepage():
     # Create the homepage window
     homepage_window = tk.Toplevel(root)
     homepage_window.title("Homepage")
-    center_window(homepage_window, 1000, 500)
+    center_window(homepage_window, 550, 360)
 
     # Load the image and keep a reference to it
-    homepage_image = PhotoImage(file="UPE-banner.png")
+    homepage_image = PhotoImage(file="UPE-background.png")
     resized_image = homepage_image.subsample(2, 2)
 
     # Create a label with the resized image
