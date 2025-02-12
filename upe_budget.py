260a2cd24071cc87abb46b16f4f460e22cc0398c
mtkaricher <98564022+mtkaricher@users.noreply.github.com>
2025-02-12 11:57:42 -0500

    Began work on budget home

--- upe_budget.py
+++ upe_budget.py
@@ -4,17 +4,34 @@ from tkinter import ttk
 from utils import center_window
 
 def open_upe_budget(homepage_window, root):
-    # Withdraws homepage
+    #Withdraws homepage
     homepage_window.withdraw()
 
+    #Create the page window
     upe_budget_window = tk.Toplevel(root)
     upe_budget_window.title("View Budget")
-    center_window(upe_budget_window, 300, 200)
+    center_window(upe_budget_window, 800, 630)
 
-    label = tk.Label(upe_budget_window, text="Records", font=("Helvetica", 40, "bold"), bd=2, relief="solid", padx=10, pady=5)
+    #label = tk.Label(upe_budget_window, text="This is Window UPE BUDGET")
+    #label.pack(pady=50)
+
+    #Add the label and buttons
+    label = tk.Label(upe_budget_window, text="Budget Home", font=("Helvetica", 40, "bold"), bd=2, relief="solid", padx=10, pady=5)
     label.place(relx=0.5, rely=0.1, anchor="center")
+    label.pack(pady=50)
 
-    btn_homepage_window = ttk.Button(upe_budget_window, text="Back to Homepage", command=lambda: [upe_budget_window.destroy(), homepage_window.deiconify()])
-    btn_homepage_window.pack(pady=10)
+    #Return button
+    btn_rtn_homepage_window = ttk.Button(upe_budget_window, text="Back to Homepage", command=lambda: [upe_budget_window.destroy(), homepage_window.deiconify()])
+    btn_rtn_homepage_window.place(relx=0.05, rely=0.05, anchor="nw")
+
+    #View history button: Open budget history window and withdraw budget home
+    btn_view_history = ttk.Button(upe_budget_window, text="View History", command=lambda: [upe_budget_window.withdraw(), homepage_window.deiconify()])
+    #btn_view_history = ttk.Button(upe_budget_window, text="View History", command=lambda: [upe_budget_window.withdraw(), open_budget_history(root)])
+    btn_view_history.place(relx=0.5, rely=0.5, anchor="center")
+
+    #Edit history button
+
+    #Input transaction button
+
 
     
